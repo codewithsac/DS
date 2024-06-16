11502557ddbe918bdeebf5afb3d1df7f45284b44
sachinincedo <33475261+sachinincedo@users.noreply.github.com>
2024-06-16 17:46:20 +0530

    I am commit this as test change

--- src/main/java/com/codewithsac/dsa/trees/BinaryTreeTest.java
+++ src/main/java/com/codewithsac/dsa/trees/BinaryTreeTest.java
@@ -3,6 +3,6 @@ package com.codewithsac.dsa.trees;
 public class BinaryTreeTest {
     public void helo(){
 
-        System.out.println("hello 3  world");
+        System.out.println("hello 1  world");
     }
 }
